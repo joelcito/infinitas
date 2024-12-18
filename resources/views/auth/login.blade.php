ef018196a9593f8eb9c143f11140893cfe1c846a
JOEL FLORES QUISPE <joel.flores@segip.gob.bo>
2024-12-18 17:29:33 -0400

    se agrego cambios para subir a pro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,21 @@
-
-
 <!DOCTYPE html>
-<!--
-Author: Keenthemes
-Product Name: Metronic
-Product Version: 8.2.3
-Purchase: https://1.envato.market/EA4JP
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
 <html lang="en">
 	<!--begin::Head-->
 	<head>
 <base href="../../../" />
-		<title>VIRUS NOT | FACTURACION</title>
+		<title>INFINITAS SOLUCIONES | FACTURACION</title>
 		<meta charset="utf-8" />
-		<meta name="description" content="The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
-		<meta name="keywords" content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta property="og:locale" content="en_US" />
-		<meta property="og:type" content="article" />
-		<meta property="og:title" content="Metronic - The World's #1 Selling Bootstrap Admin Template by KeenThemes" />
-		<meta property="og:url" content="https://keenthemes.com/metronic" />
-		<meta property="og:site_name" content="Metronic by Keenthemes" />
-		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-		<link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" />
+		<meta name="description" content="El sistema de facturación en línea más avanzado, diseñado para adaptarse a las necesidades de empresas en Bolivia y confiado por miles de usuarios. Incluye gestión completa de facturas, múltiples métodos de pago, soporte para impuestos locales y compatibilidad con facturación electrónica según las normativas bolivianas. Ideal para pymes, emprendedores y grandes empresas que buscan optimizar su gestión administrativa. Obtén acceso ahora y disfruta de la facilidad de factura." />
+        <meta name="keywords" content="sistema de facturación, sistema de inventario, facturación electrónica, software de facturación, gestión de inventario, control de stock, administración de ventas, gestión de clientes, control de proveedores, generación de reportes, impresión de facturas, soporte para impuestos, sistema en línea, facturación Bolivia, Bootstrap 5, Angular, VueJs, React, Laravel, Django, Asp.Net Core, Rails, Spring, Blazor, Node.js, administración empresarial, desarrollo web, plantillas de administración, dashboards, comercio electrónico, ERP, software empresarial" />
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1" />--}}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta property="og:locale" content="en_BO" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Sistema de Facturación e Inventario en Línea - Gestión Simplificada para Empresas" />
+		<meta property="og:url" content="https://infinitassoluciones.net/" />
+		<meta property="og:site_name" content="Sistema de Facturación e Inventario en Línea" />
+		<link rel="canonical" href="https://infinitassoluciones.net/" />
+        <link rel="shortcut icon" href="{{ asset('assets/img/logo_2.png') }}" />
 		<!--begin::Fonts(mandatory for all pages)-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 		<!--end::Fonts-->
@@ -47,7 +34,7 @@ License: For each use you must have a valid license purchased only from above li
 		<!--begin::Root-->
 		<div class="d-flex flex-column flex-root" id="kt_app_root">
 			<!--begin::Page bg image-->
-			<style>body { background-image: url('{{ asset('assets/media/auth/bg4_1.jpg') }}'); } [data-bs-theme="dark"] body { background-image: url('{{ asset('assets/media/auth/bg4-dark.jpg') }}'); }</style>
+			<style>body { background-image: url('{{ asset('assets/media/auth/bg6_1.png') }}'); } [data-bs-theme="dark"] body { background-image: url('{{ asset('assets/media/auth/bg4-dark.jpg') }}'); }</style>
 			<!--end::Page bg image-->
 			<!--begin::Authentication - Sign-in -->
 			<div class="d-flex flex-column flex-column-fluid flex-lg-row">
@@ -56,20 +43,21 @@ License: For each use you must have a valid license purchased only from above li
 					<!--begin::Aside-->
 					<div class="d-flex flex-center flex-lg-start flex-column">
 						<!--begin::Logo-->
-                        <img alt="Logo" src="{{ asset('assets/img/logo_2.png') }}" width="40%" />
+                        <img alt="Logo" src="{{ asset('assets/img/logo_2.png') }}" width="100%" />
 						<!--end::Logo-->
 						<!--begin::Title-->
                         <br>
-						<h1 class="text-white fw-normal m-0">FACTUTACIÓN EN LINEA BOLIVIA</h1>
+						<h1 class="text-white fw-normal m-0">FACTUTACIÓN EN LINEA BOLIVIA SENCILLO Y RAPIDO</h1>
 						<!--end::Title-->
 					</div>
 					<!--begin::Aside-->
 				</div>
 				<!--begin::Aside-->
 				<!--begin::Body-->
-				<div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12 p-lg-20">
+				<div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12 p-lg-20" style="align-items: center">
 					<!--begin::Card-->
-					<div class="bg-body d-flex flex-column align-items-stretch flex-center rounded-4 w-md-500px p-20">
+{{--                    <div class="bg-body d-flex flex-column align-items-stretch flex-center rounded-4 w-md-500px p-5">--}}
+                    <div class="bg-body d-flex flex-column align-items-stretch flex-center rounded-4 w-md-400px h-md-500px mb-0 p-5">
 						<!--begin::Wrapper-->
 						<div class="d-flex flex-center flex-column flex-column-fluid px-lg-10 pb-15 pb-lg-20">
 							<!--begin::Form-->
